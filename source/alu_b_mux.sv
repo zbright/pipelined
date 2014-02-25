0b801adf3e1548edd14a7a3941a37d53e7c724f8
Ziao Liang <liang37@purdue.edu>
2014-02-24 23:13:25 -0500

    fixed load forward

--- source/alu_b_mux.sv
+++ source/alu_b_mux.sv
@@ -8,6 +8,8 @@ module alu_b_mux (
             input logic [31:0] aluresult_mem_wb_output,
             input logic [31:0] upper16_ex_mem_output,
             input logic [31:0] upper16_mem_wb_output,
+            input logic [31:0] dmemload_mem_wb_output,
+            input logic [31:0] writedata_output,
             output logic [31:0] alu_b_mux_output
    );
 
@@ -18,11 +20,9 @@ always_comb
         end else if (forwardb == 3'b001) begin
             alu_b_mux_output = aluresult_ex_mem_output;
         end else if (forwardb == 3'b010) begin
-            alu_b_mux_output = aluresult_mem_wb_output;
+            alu_b_mux_output = writedata_output;
         end else if (forwardb == 3'b011) begin
             alu_b_mux_output = upper16_ex_mem_output;
-        end else if (forwardb == 3'b100) begin
-            alu_b_mux_output = upper16_mem_wb_output;
         end else begin
             alu_b_mux_output = rdat_two_id_ex_output;
         end
