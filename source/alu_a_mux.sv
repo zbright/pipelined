4fef8d14f00a6c35fa447626b6544a3e0a3709c4
Ziao Liang <liang37@purdue.edu>
2014-02-24 23:12:50 -0500

    support load forwarding

--- source/alu_a_mux.sv
+++ source/alu_a_mux.sv
@@ -8,6 +8,8 @@ module alu_a_mux (
             input logic [31:0] aluresult_mem_wb_output,
             input logic [31:0] upper16_ex_mem_output,
             input logic [31:0] upper16_mem_wb_output,
+            input logic [31:0] dmemload_mem_wb_output,
+            input logic [31:0] writedata_output,
             output logic [31:0] alu_a_mux_output
    );
 
@@ -18,11 +20,9 @@ always_comb
         end else if (forwarda == 3'b001) begin
             alu_a_mux_output = aluresult_ex_mem_output;
         end else if (forwarda == 3'b010) begin
-            alu_a_mux_output = aluresult_mem_wb_output;
+            alu_a_mux_output = writedata_output;
         end else if (forwarda == 3'b011) begin
             alu_a_mux_output = upper16_ex_mem_output;
-        end else if (forwarda == 3'b100) begin
-            alu_a_mux_output = upper16_mem_wb_output;
         end else begin
             alu_a_mux_output = rdat_one_id_ex_output;
         end
